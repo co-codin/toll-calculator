c7500a94970d72474ce566369a61d2639e95c0e0
elijah <cuiyeqing960904@gmail.com>
2024-03-11 14:41:12 -0400

    ws receiver loop

--- Makefile
+++ Makefile
@@ -2,4 +2,9 @@ obu:
 	@go build -o bin/obu obu/main.go
 	@./bin/obu
 
+receiver:
+	@go build -o bin/receiver ./data_receiver
+	@./bin/receiver
+
+
 .PHONY: obu invoicer
--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/co-codin/toll-calculator/types"
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	fmt.Println("working")
+}
+
+type DataReceiver struct {
+	msgch chan types.OBUData
+	conn  *websocket.Conn
+}
+
+func NewDataReceiver() *DataReceiver {
+	
+}
+
+func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, nil, 1028, 1028)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dr.conn = conn
+
+	go dr.wsReceiveLoop()
+}
+
+func (dr *DataReceiver) wsReceiveLoop() {
+	fmt.Println("New OBU connected client")
+	for {
+		var data types.OBUData
+		if err := dr.conn.ReadJSON(&data); err != nil {
+			log.Println("read error:", err)
+			continue
+		}
+		dr.msgch <- data
+	}
+}
