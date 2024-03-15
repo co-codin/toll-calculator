fc3fcde851eda30f3fb669e3dece0cb455fd841c
elijah <cuiyeqing960904@gmail.com>
2024-03-15 06:48:36 -0400

    building kafka roducer

---
+++ data_receiver/producer.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/co-codin/toll-calculator/types"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+type DataProducer interface {
+	ProduceData(types.OBUData) error
+}
+
+type KafkaProducer struct {
+	producer *kafka.Producer
+	topic    string
+}
+
+func NewKafkaProducer(topic string) (DataProducer, error) {
+	return nil, nil
+}
