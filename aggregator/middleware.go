8592e4ccfc0b832d8136732e48faec46ba6fd542
elijah <cuiyeqing960904@gmail.com>
2024-03-17 13:04:54 -0400

    middleware built

--- aggregator/middleware.go
+++ aggregator/middleware.go
@@ -1,5 +1,51 @@
 package main
 
-type LogMiddleware struct {
+import (
+	"time"
 
+	"github.com/co-codin/toll-calculator/types"
+	"github.com/sirupsen/logrus"
+)
+
+type LogMiddleware struct {
+	next Aggregator
+}
+
+func NewLogMiddleware(next Aggregator) Aggregator {
+	return &LogMiddleware{
+		next: next,
+	}
+}
+
+func (l *LogMiddleware) AggregateDistance(d types.Distance) (err error) {
+	defer func(start time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"took": time.Since(start),
+			"err":  err,
+		}).Info("AggregateDistance")
+	}(time.Now())
+	err = l.next.AggregateDistance(d)
+	return
+}
+
+func (m *LogMiddleware) CalculateInvoice(obuID int) (inv *types.Invoice, err error) {
+	defer func(start time.Time) {
+		var (
+			distance float64
+			amount   float64
+		)
+		if inv != nil {
+			distance = inv.TotalDistance
+			amount = inv.TotalAmount
+		}
+		logrus.WithFields(logrus.Fields{
+			"took":     time.Since(start),
+			"err":      err,
+			"obuID":    obuID,
+			"amount":   amount,
+			"distance": distance,
+		}).Info("CalculateInvoice")
+	}(time.Now())
+	inv, err = m.next.CalculateInvoice(obuID)
+	return
 }
