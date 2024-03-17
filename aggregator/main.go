16a3489bac377a47c54d278dda3bd97c41c0c64d
elijah <cuiyeqing960904@gmail.com>
2024-03-17 11:35:18 -0400

    building base handler

--- aggregator/main.go
+++ aggregator/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("working")
+
+}
+
+func handleAggregate(svc Aggregator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func makeStore() Storer {
+	storeType := os.Getenv("AGG_STORE_TYPE")
+	switch storeType {
+	case "memory":
+		return NewMemoryStore()
+	default:
+		log.Fatalf("invalid store type given %s", storeType)
+		return nil
+	}
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
