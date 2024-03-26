9fb6b88e164942528b0ff1ff58a5a7cf4027fac5
elijah <cuiyeqing960904@gmail.com>
2024-03-26 10:33:34 -0400

    grpc client

--- aggregator/client/grpcclient.go
+++ aggregator/client/grpcclient.go
@@ -1,12 +1,25 @@
 package client
 
+import (
+	"github.com/co-codin/toll-calculator/types"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 type GRPCClient struct {
 	Endpoint string
+	client   types.AggregatorClient
 }
 
-func NewGRPCClient(endpoint string) *GRPCClient {
+func NewGRPCClient(endpoint string) (*GRPCClient, error) {
+	conn, err := grpc.Dial(endpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	c := types.NewAggregatorClient(conn)
+
 	return &GRPCClient{
 		Endpoint: endpoint,
-	}
+		client:   c,
+	}, nil
 }
-
