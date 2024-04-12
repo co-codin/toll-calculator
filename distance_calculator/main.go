eddb4d6253d31ff19d6d6eae62c7d7a765ae5127
elijah <cuiyeqing960904@gmail.com>
2024-04-12 06:50:56 -0400

    gateway built

--- distance_calculator/main.go
+++ distance_calculator/main.go
@@ -19,15 +19,15 @@ func main() {
 	svc = NewCalculatorService()
 	svc = NewLogMiddleware(svc)
 
-	// httpClient := client.NewHTTPClient(aggregatorEndpoint)
+	httpClient := client.NewHTTPClient(aggregatorEndpoint)
 
-	grpcClient, err := client.NewGRPCClient(aggregatorEndpoint)
+	// grpcClient, err := client.NewGRPCClient(aggregatorEndpoint)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	kafkaConsumer, err := NewKafkaConsumer(kafkaTopic, svc, grpcClient)
+	kafkaConsumer, err := NewKafkaConsumer(kafkaTopic, svc, httpClient)
 	if err != nil {
 		log.Fatal(err)
 	}
--- gateway/main.go
+++ gateway/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/co-codin/toll-calculator/aggregator/client"
 	"github.com/sirupsen/logrus"
 )
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":6000", "the listen address of the HTTP server")
@@ -24,6 +29,40 @@ func main() {
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
 
-func handleGetInvoice(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("working"))
+type InvoiceHandler struct {
+	client client.Client
+}
+
+func newInvoiceHandler(c client.Client) *InvoiceHandler {
+	return &InvoiceHandler{
+		client: c,
+	}
+}
+
+func (h *InvoiceHandler) handleGetInvoice(w http.ResponseWriter, r *http.Request) error {
+	inv, err := h.client.GetInvoice(context.Background(), 1383987118)
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, http.StatusOK, inv)
+}
+
+func writeJSON(w http.ResponseWriter, code int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeAPIFunc(fn apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(start time.Time) {
+			logrus.WithFields(logrus.Fields{
+				"took": time.Since(start),
+				"uri":  r.RequestURI,
+			}).Info("REQ :: ")
+		}(time.Now())
+		if err := fn(w, r); err != nil {
+			writeJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+	}
 }
