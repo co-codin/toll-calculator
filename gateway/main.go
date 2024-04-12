e4e4eb61abf1e39f47aa75e9907c4f453a2abfdd
elijah <cuiyeqing960904@gmail.com>
2024-04-12 05:56:11 -0400

    building http gateway

--- gateway/main.go
+++ gateway/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/co-codin/toll-calculator/aggregator/client"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	listenAddr := flag.String("listenAddr", ":6000", "the listen address of the HTTP server")
+	aggregatorServiceAddr := flag.String("aggServiceAddr", "http://localhost:3000", "the listen address of the aggregator service")
+	flag.Parse()
+
+	var (
+		client     = client.NewHTTPClient(*aggregatorServiceAddr) // endpoint of the aggregator service
+		invHandler = newInvoiceHandler(client)
+	)
+
+	http.HandleFunc("/invoice", makeAPIFunc(invHandler.handleGetInvoice))
+	logrus.Infof("gateway HTTP server running on port %s", *listenAddr)
+	log.Fatal(http.ListenAndServe(*listenAddr, nil))
+}
+
+func handleGetInvoice(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("working"))
+}
